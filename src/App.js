ac6555c6d74b2a7580fdccc85993058e20189af7
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-06 15:33:35 +0000

    fixed layout

--- src/App.js
+++ src/App.js
@@ -8,20 +8,19 @@ function App() {
 
       <div className="container">
       <div className="intro">
-      Welcome to the quote generator. Every 10 seconds, you'll be delivered with a new, inspiring quote from a legendary person.
+      <h2>Welcome to the quote generator. Every 10 seconds, you'll be delivered with a new, inspiring quote from a legendary person.</h2>
       </div>
       <div className="pic">
         <img src={face} alt=''/>
       </div>
       <div className="quote">
-      "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+      "CSS can do one"
       </div>
         <div className="name">
-          Name here
+          Cameron Blackwood (name div)
         </div>
       <div className="bio">
-        Bio goes here
+        Cameron Blackwood is a 24 year old software engineer from London, read more about him
       </div>
       </div>
     </div>
