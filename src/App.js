5fb753511cecb4df807943b5a99a8e7bf4ba44b8
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-06 12:05:03 +0000

    grid added

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="intro">
-      Welcome to the sports quote generator. Every 10 seconds, you'll be delivered with a new, inspiring quote from a legendary athelete.
-      </div>
+
       <div className="container">
+      <div className="intro">
+      Welcome to the quote generator. Every 10 seconds, you'll be delivered with a new, inspiring quote from a legendary person.
+      </div>
       <div className="pic">
         Picture will go here
       </div>
       <div className="quote">
         Quote will go here
       </div>
+        <div className="name">
+          Name here
+        </div>
       <div className="bio">
         Bio goes here
-      </div>
-      <div className="wiki">
-        Link to Wikipedia goes here
       </div>
       </div>
     </div>
